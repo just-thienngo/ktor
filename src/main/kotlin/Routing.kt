f889235f3198bf169d686ca915885e5e7240daa3
Chi Thien Ngo <thienngo2003@gmail.com>
2025-07-28 18:38:28 +0700

    Create the static content

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -8,12 +8,16 @@ import com.tkjen.utils.tasksAsTable
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.request.receive
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+
+        staticResources("/task-ui", "task-ui")
+
         get("/") {
             call.respondText("Hello World!")
         }
